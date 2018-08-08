0611f72630d62a21b66430633429d49754cadae0
森本 <mrmt0814@gmail.com>
2018-08-08 21:25:37 +0900

    基本動作の実装

---
+++ GameBattle/GameBattle/Code/Player.cpp
@@ -0,0 +1,42 @@
+#include "Player.h"
+
+
+GameObject::Player::Player(int id)
+{
+	_id = id;
+
+	_pos = Point(100, 100);
+	_velocity = Point::Zero;
+	_size = Point(48, 64);
+	_tag = L"Player[" + ToString(id) + L"]";
+}
+
+
+void GameObject::Player::update()
+{
+	_velocity.x = 0;
+	if (Input::KeyD.pressed) { _velocity.x = +4; }
+	if (Input::KeyA.pressed) { _velocity.x = -4; }
+
+	_velocity.y += 0.6;
+	if (Input::KeyW.clicked) { _velocity.y = -12; }
+
+	moveObject(true);
+}
+
+
+void GameObject::Player::draw() const
+{
+	getCollider().draw(Palette::Orange);
+}
+
+
+bool GameObject::Player::eraser() const
+{
+	return false;
+}
+
+
+void GameObject::Player::collisionUpdate(const String &)
+{
+}
