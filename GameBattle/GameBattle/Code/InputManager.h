c13cb74a507acaab385d0af245dcd31fac96e1c8
森本 <mrmt0814@gmail.com>
2018-08-24 14:48:47 +0900

    シングルトンに設計

--- GameBattle/GameBattle/Code/InputManager.h
+++ GameBattle/GameBattle/Code/InputManager.h
@@ -9,6 +9,30 @@ namespace GameData
 
 	class InputManager
 	{
+	private:
+
+	private:
+
+		InputManager();
+
+	public:
+
+		InputManager(const InputManager&)            = delete;
+		InputManager& operator=(const InputManager&) = delete;
+		InputManager(InputManager&&)                 = delete;
+		InputManager& operator=(InputManager&&)      = delete;
+
+	public:
+
+		static InputManager & Instance()
+		{
+			static InputManager instatnce;
+			return instatnce;
+		}
+
+	public:
+
+
 
 	};
 
