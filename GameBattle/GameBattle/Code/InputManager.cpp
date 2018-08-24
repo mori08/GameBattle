35304a8888e9d154059fa5d72791a2fc194fd5c5
森本 <mrmt0814@gmail.com>
2018-08-25 02:18:18 +0900

    入力を取得する関数の実装

---
+++ GameBattle/GameBattle/Code/InputManager.cpp
@@ -0,0 +1,139 @@
+#include "InputManager.h"
+
+
+bool GameData::InputManager::get(int id, const Button & button, const InputType & inputType)
+{
+	if (id < 0 || id >= PLAYER_NUM)
+	{
+#ifdef _DEBUG
+		Println(L"error > InputManager‚É—^‚¦‚ç‚ê‚½id‚ª•s“KØ‚Å‚·");
+#endif // _DEBUG
+
+		return keyInput(button, inputType);
+	}
+
+	if (Gamepad(id).isConnected())
+	{
+		return instatnce(id).padInput(Gamepad(id), button, inputType);
+	}
+
+	return keyInput(button, inputType);
+}
+
+
+void GameData::InputManager::updatePreVec()
+{
+	for (int i = 0; i < PLAYER_NUM; ++i)
+	{
+		if (Gamepad(i).isConnected())
+		{
+			instatnce(i).preVec = Vec2(Gamepad(i).x, Gamepad(i).y);
+		}
+	}
+}
+
+
+bool GameData::InputManager::keyInput(const Button & button, const InputType & inputType)
+{
+	Key key;
+
+	switch(button)
+	{
+	case Button::Left:  key = Input::KeyA;         break;
+
+	case Button::Right: key = Input::KeyD;         break;
+
+	case Button::Up:    key = Input::KeyW;         break;
+
+	case Button::Down:  key = Input::KeyS;         break;
+
+	case Button::One:   key = Input::KeyO;         break;
+		
+	case Button::Two:   key = Input::KeySemicolon; break;
+
+	case Button::Three: key = Input::KeyL;         break;
+
+	case Button::Four:  key = Input::KeyK;         break;
+
+	default: return false;
+	}
+
+	switch (inputType)
+	{
+	case InputType::Clicked:  return key.clicked;
+
+	case InputType::Pressed:  return key.pressed;
+
+	case InputType::Released: return key.released;
+	}
+
+	return false;
+}
+
+
+bool GameData::InputManager::padInput(const Gamepad & pad, const Button & button, const InputType & inputType)
+{
+	Key key;
+
+	switch (button)
+	{
+	case Button::Left:  key = pad.povLeft;     break;
+
+	case Button::Right: key = pad.povRight;    break;
+
+	case Button::Up:    key = pad.povForward;  break;
+
+	case Button::Down:  key = pad.povBackward; break;
+
+	case Button::One:   key = pad.button(0);   break;
+
+	case Button::Two:   key = pad.button(1);   break;
+
+	case Button::Three: key = pad.button(2);   break;
+
+	case Button::Four:  key = pad.button(3);   break;
+
+	default: return false;
+	}
+
+	bool analogFlag = false;
+
+	switch(button)
+	{
+	case Button::Left:  analogFlag = analogInput(-pad.x, -preVec.x, inputType); break;
+
+	case Button::Right: analogFlag = analogInput( pad.x,  preVec.x, inputType); break;
+	
+	case Button::Up:    analogFlag = analogInput(-pad.y, -preVec.y, inputType); break;
+	
+	case Button::Down:  analogFlag = analogInput( pad.y,  preVec.y, inputType); break;
+	}
+
+	switch (inputType)
+	{
+	case InputType::Clicked:  return key.clicked  || analogFlag;
+
+	case InputType::Pressed:  return key.pressed  || analogFlag;
+
+	case InputType::Released: return key.released || analogFlag;
+	}
+
+	return false;
+	
+}
+
+
+bool GameData::InputManager::analogInput(double value, double preValue, InputType inputType)
+{
+	bool input    = value    > 0.9;
+	bool preInput = preValue > 0.9;
+
+	switch (inputType)
+	{
+	case InputType::Pressed:  return  input;
+	case InputType::Clicked:  return  input && !preInput;
+	case InputType::Released: return !input &&  preInput;
+	}
+
+	return false;
+}
--- GameBattle/GameBattle/Code/InputManager.h
+++ GameBattle/GameBattle/Code/InputManager.h
@@ -7,32 +7,96 @@
 namespace GameData
 {
 
+	enum class Button
+	{
+		Left,
+		Right,
+		Up,
+		Down,
+		One,
+		Two,
+		Three,
+		Four
+	};
+
+	enum class InputType
+	{
+		Clicked,
+		Pressed,
+		Released
+	};
+
 	class InputManager
 	{
 	private:
 
+		static const int PLAYER_NUM = 4; // プレイヤーの人数
+
+		Vec2 preVec; // 一フレーム前のアナログパッドの動き
+
 	private:
 
 		InputManager();
-
-	public:
 
 		InputManager(const InputManager&)            = delete;
 		InputManager& operator=(const InputManager&) = delete;
 		InputManager(InputManager&&)                 = delete;
 		InputManager& operator=(InputManager&&)      = delete;
 
-	public:
-
-		static InputManager & Instance()
+		/// <summary>
+		/// インスタンスの取得
+		/// </summary>
+		/// <param name="id"> プレイヤーの識別番号 </param>
+		/// <returns> インスタンス </returns>
+		static InputManager & instatnce(int id)
 		{
-			static InputManager instatnce;
-			return instatnce;
+			static InputManager instance[PLAYER_NUM];
+			return instance[id];
 		}
 
 	public:
 
+		/// <summary>
+		/// 入力を取得します。
+		/// </summary>
+		/// <param name="id"> プレイヤーの識別番号 </param>
+		/// <param name="button"> どのボタンの入力か </param>
+		/// <param name="inputType"> どのような入力か </param>
+		/// <returns> 入力があったとき true , そうでないとき false </returns>
+		static bool get(int id, const Button & button, const InputType & inputType);
 
+		/// <summary>
+		/// アナログパッドの倒されている方向を更新します。
+		/// </summary>
+		static void updatePreVec();
+
+	private:
+
+		/// <summary>
+		/// キーボードでの入力を管理します。
+		/// </summary>
+		/// <param name="button"> どのボタンの入力か </param>
+		/// <param name="inputType"> どのような入力か </param>
+		/// <returns> 入力があったとき true , そうでないとき false </returns> 
+		static bool keyInput(const Button & button, const InputType & inputType);
+
+		/// <summary>
+		/// ゲームパッドでの入力を取得します。
+		/// </summary>
+		/// <param name="pad"> ゲームパッド </param>
+		/// <param name="button"> どのボタンの入力か </param>
+		/// <param name="inputType"> どのような入力か </param>
+		/// <returns> 入力があったとき true , そうでないとき false </returns>
+		bool padInput(const Gamepad & pad, const Button & button, const InputType & inputType);
+
+		/// <summary>
+		/// アナログパッドの入力を2値で取得します。
+		/// </summary>
+		/// <param name="value"> 値 </param>
+		/// <param name="preValue"> 一フレーム前の値 </param>
+		/// <param name="inputType"> どのような入力か </param>
+		/// <returns> 入力があったとき true , そうでないとき false </returns>
+		bool analogInput(double value, double preValue, InputType inputType);
 
 	};
 
